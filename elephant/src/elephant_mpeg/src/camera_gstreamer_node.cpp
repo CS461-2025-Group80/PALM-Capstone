a84b0bc5f9ba7a895604631905fd2a789fb8a13b
Luke <hashbarl@oregonstate.edu>
2026-02-04 18:30:22 -0800

    Camera node running with opencv and removed opencv from gstreamer node

--- elephant/src/elephant_mpeg/src/camera_gstreamer_node.cpp
+++ elephant/src/elephant_mpeg/src/camera_gstreamer_node.cpp
@@ -1,72 +1,70 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
-#include <cv_bridge/cv_bridge.h>
-#include <opencv2/opencv.hpp>
-
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
 
-class GStreamerCameraNode : public rclcpp::Node {
+class GStreamerEncoderNode : public rclcpp::Node
+{
 public:
-    GStreamerCameraNode() : Node("gstreamer_camera_node") {
-        // Subscribe to ROS2 camera topic
-        sub_ = this->create_subscription<sensor_msgs::msg::Image>(
-            "/camera/color/image_raw", 10,
-            std::bind(&GStreamerCameraNode::imageCallback, this, std::placeholders::_1)
+    GStreamerEncoderNode() : Node("gstreamer_encoder_node")
+    {
+        sub_ = create_subscription<sensor_msgs::msg::Image>(
+            "/camera/processed", 10,
+            std::bind(&GStreamerEncoderNode::callback, this, std::placeholders::_1)
         );
 
-        // Initialize GStreamer pipeline
         gst_init(nullptr, nullptr);
 
-        std::string pipeline_str =
-            "appsrc name=mysrc ! videoconvert ! "
+        pipeline_ = gst_parse_launch(
+            "appsrc name=src is-live=true format=time ! "
+            "video/x-raw,format=BGR ! "
+            "videoconvert ! "
             "timeoverlay ! "
             "avenc_mpeg2video bitrate=4000000 ! "
-            "mpegpsmux ! filesink location=myagv_video.mpg";
+            "mpegpsmux ! filesink location=myagv_video.mpg",
+            nullptr
+        );
 
-        GError* error = nullptr;
-        pipeline_ = gst_parse_launch(pipeline_str.c_str(), &error);
-        if (!pipeline_) {
-            RCLCPP_ERROR(this->get_logger(), "Failed to create GStreamer pipeline");
-            exit(-1);
-        }
+        appsrc_ = GST_APP_SRC(
+            gst_bin_get_by_name(GST_BIN(pipeline_), "src"));
 
-        appsrc_ = GST_APP_SRC(gst_bin_get_by_name(GST_BIN(pipeline_), "mysrc"));
         gst_element_set_state(pipeline_, GST_STATE_PLAYING);
     }
 
-    ~GStreamerCameraNode() {
+    ~GStreamerEncoderNode()
+    {
         gst_element_set_state(pipeline_, GST_STATE_NULL);
         gst_object_unref(pipeline_);
     }
 
 private:
-    void imageCallback(const sensor_msgs::msg::Image::SharedPtr msg) {
-        cv::Mat frame = cv_bridge::toCvShare(msg, "bgr8")->image;
+    void callback(const sensor_msgs::msg::Image::SharedPtr msg)
+    {
+        GstBuffer *buffer =
+            gst_buffer_new_allocate(nullptr, msg->data.size(), nullptr);
 
-        // Wrap OpenCV Mat into GstBuffer
-        int size = frame.total() * frame.elemSize();
-        GstBuffer* buffer = gst_buffer_new_allocate(nullptr, size, nullptr);
-        gst_buffer_fill(buffer, 0, frame.data, size);
+        gst_buffer_fill(buffer, 0, msg->data.data(), msg->data.size());
 
-        // Set PTS/DTS from ROS timestamp
-        GST_BUFFER_PTS(buffer) = msg->header.stamp.sec * 1e9 + msg->header.stamp.nanosec;
-        GST_BUFFER_DTS(buffer) = GST_BUFFER_PTS(buffer);
+        GstClockTime ts =
+            msg->header.stamp.sec * 1000000000LL +
+            msg->header.stamp.nanosec;
 
-        GstFlowReturn ret;
-        g_signal_emit_by_name(appsrc_, "push-buffer", buffer, &ret);
+        GST_BUFFER_PTS(buffer) = ts;
+        GST_BUFFER_DTS(buffer) = ts;
+
+        g_signal_emit_by_name(appsrc_, "push-buffer", buffer, nullptr);
         gst_buffer_unref(buffer);
     }
 
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr sub_;
-    GstElement* pipeline_;
-    GstAppSrc* appsrc_;
+    GstElement *pipeline_;
+    GstAppSrc *appsrc_;
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<GStreamerCameraNode>();
-    rclcpp::spin(node);
+    rclcpp::spin(std::make_shared<GStreamerEncoderNode>());
     rclcpp::shutdown();
     return 0;
 }
